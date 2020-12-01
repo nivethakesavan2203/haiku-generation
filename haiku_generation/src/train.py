474f5c0bd5543e613d9a82f23f8ad5762c66c90d
nivethakesavan <nivetha.kesavan@colorado.edu>
2020-12-01 04:08:55 -0700

    Evaluation metric added

--- haiku_generation/src/train.py
+++ haiku_generation/src/train.py
@@ -1,3 +1,5 @@
+import collections
+import nltk
 import keras.utils as utils
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -74,19 +76,64 @@ def haiku_generation(start_word, total_words, tokenizer, X, lstm_model):
 
     return start_word
 
+def evaluation(haiku_data, generated_haiku):
+    #using preplexity to evaluate
+    #evaluation code from https://stackoverflow.com/questions/33266956/nltk-package-to-estimate-the-unigram-perplexity
+    tokens_list = []
+    for haiku in haiku_data:
+        tokens_list.append(nltk.word_tokenize(haiku))
+    tokens = [item for sublist in tokens_list for item in sublist]
+    # here you construct the unigram language model
+    def unigram(tokens):
+        model = collections.defaultdict(lambda: 0.01)
+        for f in tokens:
+            try:
+                model[f] += 1
+            except KeyError:
+                model[f] = 1
+                continue
+        N = float(sum(model.values()))
+        for word in model:
+            model[word] = model[word] / N
+        return model
+
+    # computes perplexity of the unigram model on a testset
+    def perplexity(testset, model):
+        testset = testset.split()
+        perplexity = 1
+        N = 0
+        for word in testset:
+            N += 1
+            perplexity = perplexity * (1 / model[word])
+        perplexity = pow(perplexity, 1 / float(N))
+        return perplexity
+
+    model = unigram(tokens)
+    print("Perplexity score:")
+    print(perplexity(generated_haiku, model))
 
 if __name__ == '__main__':
-    haikus = HaikuDataset('/home/peasant98/Documents/haiku-generation/haiku_generation/datasets/all_haiku.csv', is_kaggle=True)
+    haikus = HaikuDataset('all_haiku.csv', is_kaggle=True)
     haiku_data = haikus.get_all_poems()[:1000]
     tokenizer = Tokenizer()
-
     sequences, num_words = get_n_gram_text_sequences(tokenizer, haiku_data)
+
 
     X, Y = pad_text_sequences(num_words, sequences)
 
     lstm_model = get_lstm_model(num_words=num_words, X=X)
     lstm_model.fit(X, Y, epochs=100, batch_size=32, verbose=1)
 
-    haiku = haiku_generation("wind", 9, tokenizer, X, lstm_model)
-    # TODO -- demonstrating explainability of the model
+    """haiku = haiku_generation("wind", 9, tokenizer, X, lstm_model)
     print(haiku)
+    evaluation(haiku_data, haiku)"""
+
+    start_words = ['wind', 'morning', 'twilight', 'water', 'snow', 'I']
+    for word in start_words:
+        haiku = haiku_generation(word, 9, tokenizer, X, lstm_model)
+        # TODO -- demonstrating explainability of the model
+        print(haiku)
+        evaluation(haiku_data, haiku)
+
+
+
